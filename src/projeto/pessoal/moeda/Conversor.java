f0f6a314f6e091ccfc04f47f774b72341a454716
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-02 01:45:51 -0300

    mensagem

--- src/projeto/pessoal/moeda/Conversor.java
+++ src/projeto/pessoal/moeda/Conversor.java
@@ -11,7 +11,21 @@ public class Conversor{
     
     }
     public static void main(String[] args) {
+        boolean valormoeda = true;
         Scanner scan = new Scanner(System.in);
+
+        while (valormoeda) {
+        System.out.println("\n escolha uma opção: ");
+        System.out.println("01. Euro");
+        System.out.println("02. Libra Estrelina");
+        System.out.println("03. Iene Japonês");
+        System.out.println("04. Dólar Canadense");
+        System.out.println("05. Franco Suíço");
+            
+        }
+        
+
+
 
         scan.close();
     }
