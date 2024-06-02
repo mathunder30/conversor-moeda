1293ef88c4531b4014e2ccaa215225000481124b
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-02 01:36:14 -0300

    conversor-de-moedas-java

---
+++ src/projeto/pessoal/moeda/Conversor.java
@@ -0,0 +1,19 @@
+package projeto.pessoal.moeda;
+
+import java.util.Scanner;
+
+
+
+public class Conversor{
+    public static double EUR(double moeda1, double moeda2){
+    
+        return moeda1 * moeda2;
+    
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        scan.close();
+    }
+
+}
